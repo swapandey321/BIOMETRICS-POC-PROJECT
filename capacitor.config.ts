efcf7526bb1c9cfbf31b781e1e07dca1e9e3fbca
ashutosh4023 <ashutosh.ray10@yahoo.co.in>
2025-07-11 11:11:53 -0700

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.poc.bio.app',
   appName: 'biometrics-poc-project',
-  webDir: 'dist/biometrics-poc-project/browser'
+  webDir: 'dist/biometrics-poc-project/browser',
+  plugins: {
+    CapacitorHttp:{
+      enabled: true
+    }
+  }
 };
 
 export default config;
